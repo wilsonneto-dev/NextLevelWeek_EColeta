8815ef38998adddd2781b67a522e425ad74d18d8
Wilson Neto <contato@wilsonneto.com.br>
2020-06-02 13:54:25 -0300

    migrations: relantionship

--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -3,8 +3,8 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
   return knex.schema.createTable('point_items', (table) => {
     table.increments('id').primary();
-    table.integer('point_id').notNullable();
-    table.integer('item_id').notNullable();
+    table.integer('point_id').notNullable().references('id').inTable('points');
+    table.integer('item_id').notNullable().references('id').inTable('item');
   });
 }
 
